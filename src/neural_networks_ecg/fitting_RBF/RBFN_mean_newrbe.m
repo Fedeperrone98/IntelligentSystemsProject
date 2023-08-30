255263e1dd784fa214133a2a6c6ac88056f38bb7
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-08-30 16:22:17 +0200

    start rbfn

---
+++ src/neural_networks_ecg/fitting_RBF/RBFN_mean_newrbe.m
@@ -0,0 +1,41 @@
+%%
+clear
+close all 
+clc
+
+%% Load input and target data
+load("saves/BEFORE_TRAINING.mat");
+INPUT = INPUT_MEAN;
+TARGET = TARGET_MEAN;
+
+%% distance between inputs
+dist = pdist(INPUT');
+fprintf('Min distance between adjacent input vectors: %f\n', min(dist));
+fprintf('Max distance across whole input space: %f\n', max(dist));
+
+%% Newrbe 
+
+% Choose a spread larger than the distance between adjacent input (0.000955),
+% but smaller then the distance across the whole input space (1.796102)
+spread = 0.5;
+
+% Create Radial Basis Function network
+net = newrbe(INPUT, TARGET, spread);
+
+% View the Network
+view(net)
+
+% simulate a network over complete input range
+Y = net(INPUT);
+
+% plot network response
+perf = perform(net, TARGET, Y);
+fprintf('Error after newrbe: %f (%e)\n', ...
+        perf, perf);
+
+regression_model = fitlm(TARGET, Y);
+fprintf('Rsquared after newrb: %f\n', ...
+        regression_model.Rsquared.Ordinary);
+
+figure; plotregression(TARGET, Y)
+   
--- src/neural_networks_ecg/fitting_RBF/RBF_mean.m
+++ src/neural_networks_ecg/fitting_RBF/RBF_mean.m
@@ -2,3 +2,63 @@
 clear
 close all 
 clc
+
+%% Load input and target data
+load("saves/BEFORE_TRAINING.mat");
+INPUT = INPUT_MEAN;
+TARGET = TARGET_MEAN;
+
+%% distance between inputs
+dist = pdist(INPUT');
+fprintf('Min distance between adjacent input vectors: %f\n', min(dist));
+fprintf('Max distance across whole input space: %f\n', max(dist));
+
+%% Radial Basis Function (newrb)
+
+% Set network parameters
+error_goal = 0;
+max_neurons = 30;
+
+% Choose a spread larger than the distance between adjacent input (0.000955),
+% but smaller then the distance across the whole input space (1.796102)
+spread = 0.45;
+
+
+% Create Radial Basis Function network
+net = newrb(INPUT, TARGET, error_goal, spread, max_neurons, 10);
+
+% Print performance and Rsquared
+Y = net(INPUT);
+perf = perform(net, TARGET, Y);
+fprintf('Error after newrb: %f (%e)\n', ...
+        perf, perf);
+regression_model = fitlm(TARGET, Y);
+fprintf('Rsquared after newrb: %f\n', ...
+        regression_model.Rsquared.Ordinary);
+figure; plotregression(TARGET, Y)
+   
+%% Bayesian regularization (trainbr)
+
+% Retrain RBF by performing Levenberg-Marquardt training with Bayesian
+% regularization, to improve performance
+net.trainFcn = 'trainbr';
+net.trainParam.epochs = 100;
+
+net.trainParam.show = 20;
+net.trainParam.showCommandLine = 1;
+net.trainParam.showWindow = 0;
+
+[net, tr_after_reg] = train(net, INPUT, TARGET, ...
+                            'useParallel','yes');
+
+% Print performance and Rsquared
+fprintf('Error after training with TRAINBR: %f (%e)\n', ...
+        tr_after_reg.best_perf, tr_after_reg.best_perf);
+Y = net(INPUT);
+regression_model = fitlm(TARGET, Y);
+fprintf('Rsquared after training with TRAINBR: %f\n', ...
+        regression_model.Rsquared.Ordinary);
+ 
+% Plot performance and regression 
+figure; plotperform(tr_after_reg);
+figure; plotregression(TARGET, Y);
